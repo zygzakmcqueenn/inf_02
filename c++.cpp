679a0f951b530767ad9f6dab4d9c1600122dde18
zygzakmcqueenn <jankrzysztofwozniak@gmail.com>
2023-10-25 14:07:52 +0200

    c++ dodany

---
+++ c++.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+using namespace std;
+
+
+int main(){
+short liczba = 3;
+float zmiennoprzecinkowa = 3.525;
+string tekst = "tekst";
+char znak = 'A';
+bool znakowy = false;
+
+
+cout << "szubidubi meow";
+cout << endl;
+cout << "sdfv" << endl;
+cout << "liczba calkowita: " << liczba << endl;
+
+string jakos;
+cout << "Podaj jakis wyraz ";
+cin >> jakos;
+cout << "Wprowadzony wyraz: " << jakos;
+
+
+	
+  return 0;
+}
