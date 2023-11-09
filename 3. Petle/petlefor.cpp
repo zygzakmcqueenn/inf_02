8f5e7e09651c01974a5cac4457022fa40b66bd84
zygzakmcqueenn <jankrzysztofwozniak@gmail.com>
2023-11-09 15:03:31 +0100

    szubidubi

---
+++ 3. Petle/petlefor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+   for(int i=1; i < 11; i++){
+       for(int j=0; j < 11; j++){
+	   cout << endl;
+           cout << i << " * " << j << " = " << i*j << endl;
+       }
+       cout << endl;
+   }
+}
